c6ac99017a91b311a10cc63fc98ca713fd74aee0
Chaz Kiker <70249988+chazkiker2@users.noreply.github.com>
2021-06-09 21:56:47 -0500

    initial project set up (#1)
    * add high-level README.md
    
    * try alt inline styles
    
    * remove inline styles from README
    
    * set up initial TS app

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    env: {
+        node: true,
+        commonjs: true,
+    },
+    rules: {
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                args: "none",
+                argsIgnorePattern: "^_",
+                caughtErrors: "none",
+                ignoreRestSiblings: true,
+            },
+        ],
+    },
+};
